8a752c2b55884429d8197eb5adecdff691d03452
sato0w0 <130539468+sato0w0@users.noreply.github.com>
2023-06-11 14:03:45 +0000

    [Add]料理注文システム

--- menu.rb
+++ menu.rb
@@ -1,24 +1,33 @@
+require "date"
+
 class Menu
   attr_accessor :name
   attr_accessor :price
 
-  # 商品の情報
-  def initialize(name:,price:)
+  def initialize(name:, price:)
     self.name = name
     self.price = price
   end
-
-  # 商品の情報表示用
+  
   def info
     return "#{self.name} #{self.price}円"
   end
   
-  # 合計金額の算出用
   def get_total_price(count)
     total_price = self.price * count
     if count >= 3
-      return total_price - 100
+      total_price -= 100
     end
+    
+    if count >= 1 && Menu.is_discount_day?
+      total_price -= 100
+    end
+    
     return total_price
+  end
+  
+  def Menu.is_discount_day?
+    today = Date.today
+    return today.sunday?
   end
 end
